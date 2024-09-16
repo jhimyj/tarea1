9e8a2c81521fe4c6063e0d5150e7b5a155426298
jhimy <jhimy.delgado@utec.edu.pe>
2024-09-15 23:33:58 -0500

    tarea terminada

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 import os
+import sys
+from aws_cdk import App
+from aws_cdk import DefaultStackSynthesizer
+from tarea1.tarea1_stack import Tarea1Stack  # Asegúrate de que el nombre del archivo sea correcto
 
-import aws_cdk as cdk
+app = App()
 
-from tarea1.tarea1_stack import Tarea1Stack
+default_stack_synthesizer = DefaultStackSynthesizer(
+    file_assets_bucket_name="cdk-${Qualifier}-assets-${AWS::AccountId}-${AWS::Region}",
+    bucket_prefix="",
+    image_assets_repository_name="cdk-${Qualifier}-container-assets-${AWS::AccountId}-${AWS::Region}",
+    deploy_role_arn="arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+    file_asset_publishing_role_arn="arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+    image_asset_publishing_role_arn="arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+    cloud_formation_execution_role="arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+    lookup_role_arn="arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+    bootstrap_stack_version_ssm_parameter="/cdk-bootstrap/${Qualifier}/version",
+    generate_bootstrap_version_rule=True,
+)
 
-
-app = cdk.App()
-Tarea1Stack(app, "Tarea1Stack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+Tarea1Stack(app, "AwsCdkHackForAwsAcademyLearnerLabStack", synthesizer=default_stack_synthesizer)
 
 app.synth()
--- tarea1/tarea1_stack.py
+++ tarea1/tarea1_stack.py
@@ -1,19 +1,93 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    CfnOutput
 )
 from constructs import Construct
 
 class Tarea1Stack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Definir el VPC
+        vpc = ec2.Vpc(self, "MyVpc", max_azs=3)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "Tarea1Queue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Definir el rol IAM
+        role = iam.Role.from_role_arn(self, "LabRole", "arn:aws:iam::086310855935:role/LabRole")
+
+        # AMI y tipo de instancia
+        machine_image = ec2.MachineImage.generic_linux({
+            'us-east-1': 'ami-0aa28dab1f2852040'
+        })
+        instance_type = ec2.InstanceType.of(
+            ec2.InstanceClass.BURSTABLE2, 
+            ec2.InstanceSize.MICRO
+        )
+        
+        # Crear el grupo de seguridad
+        security_group = ec2.SecurityGroup(self, "MySecurityGroup",
+            vpc=vpc,
+            description="Allow SSH and HTTP access",
+            allow_all_outbound=True
+        )
+        security_group.add_ingress_rule(
+            ec2.Peer.any_ipv4(), 
+            ec2.Port.tcp(22), 
+            "Permitir acceso SSH"
+        )
+        security_group.add_ingress_rule(
+            ec2.Peer.any_ipv4(), 
+            ec2.Port.tcp(80), 
+            "Permitir acceso HTTP"
+        )
+
+        # Referenciar el par de llaves existente
+        key_pair = ec2.KeyPair.from_key_pair_name(self, "KeyPair", "vockey")
+
+        # Crear la instancia EC2
+        instance = ec2.Instance(self, "EC2Instance",
+            instance_type=instance_type,
+            machine_image=machine_image,
+            vpc=vpc,
+            role=role,
+            security_group=security_group,
+            vpc_subnets={"subnet_type": ec2.SubnetType.PUBLIC},
+            key_pair=key_pair,  # Usar el par de llaves referenciado
+            block_devices=[ec2.BlockDevice(
+                device_name="/dev/sda1",
+                volume=ec2.BlockDeviceVolume.ebs(20)  # Tamaño del volumen de acuerdo al .yml
+            )]
+        )
+
+        # Script de usuario para clonar repositorios
+        user_data = ec2.UserData.custom("""
+            #!/bin/bash
+            cd /var/www/html/
+            git clone https://github.com/utec-cc-2024-2-test/websimple.git
+            git clone https://github.com/utec-cc-2024-2-test/webplantilla.git
+            ls -l
+        """)
+        instance.add_user_data(user_data.render())
+
+        # Salidas del stack
+        CfnOutput(self, "InstanceId",
+            value=instance.instance_id,
+            description="ID de la instancia EC2"
+        )
+
+        CfnOutput(self, "InstancePublicIP",
+            value=instance.instance_public_ip,
+            description="IP pública de la instancia"
+        )
+
+        CfnOutput(self, "websimpleURL",
+            value=f"http://{instance.instance_public_ip}/websimple",
+            description="URL de websimple"
+        )
+
+        CfnOutput(self, "webplantillaURL",
+            value=f"http://{instance.instance_public_ip}/webplantilla",
+            description="URL de webplantilla"
+        )
